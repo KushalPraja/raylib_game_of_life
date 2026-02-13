df45d535318d69698c8a62676d0680b6099f3592
Kushal Prajapati <KPrajapati@kaimaging.com>
2026-02-13 11:22:07 -0500

    push

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include "raylib.h"
+#include "raymath.h"
 #include <numbers>
 #include <string>
 #include <vector>
-#include "raymath.h"
-
 
 struct Tile {
   float width;
@@ -12,7 +11,6 @@ struct Tile {
   Color color;
   Color BorderColor;
 };
-
 
 void DrawTile(Tile tile) {
   DrawRectangle(tile.position.x, tile.position.y, tile.width, tile.height,
@@ -83,11 +81,14 @@ bool checkNeighbours(const Grid &grid, int row, int col) {
 
 void CellularAutomation(Grid &grid) {
 
+  // temp is the new grid that will replace the old grid
   std::vector<std::vector<Tile>> temp;
   for (int row = 0; row < grid.rows; row++) {
     std::vector<Tile> tempRow;
     for (int col = 0; col < grid.cols; col++) {
       Tile &tile = grid.tiles[row][col];
+
+      // update the states based on the neighbours
       if (checkNeighbours(grid, row, col)) {
         tempRow.push_back(
             {tile.width, tile.height, tile.position, BLACK, GRAY});
@@ -135,8 +136,8 @@ int main() {
   SetTargetFPS(60);
 
   Camera2D camera{};
-  camera.target = (Vector2){worldWidth/2.0f, worldHeight/2.0f};
-  camera.offset = (Vector2){screenWidth/2.0f, screenHeight/2.0f};
+  camera.target = (Vector2){worldWidth / 2.0f, worldHeight / 2.0f};
+  camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
   camera.rotation = 0.0f;
   camera.zoom = 1.0f;
 
@@ -147,12 +148,18 @@ int main() {
   Grid grid = PixelGrid(worldWidth, worldHeight, tilePixelSize);
 
   Vector2 prevMousePos = GetMousePosition();
+  float timer = 0.0f;
+
+  float updateInterval = 1.0f;
+  Clamp(updateInterval, 0.1f, 10.0f);
 
   while (!WindowShouldClose()) {
 
+    timer += GetFrameTime();
+
     float mouseDelta = GetMouseWheelMove();
     float newZoom = camera.zoom + mouseDelta * 0.1f;
-    if (newZoom <= 0){
+    if (newZoom <= 0) {
       newZoom = 0.1f;
     }
     camera.zoom = newZoom;
@@ -162,11 +169,21 @@ int main() {
     prevMousePos = thisPos;
 
 
-    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
-      camera.target = Vector2Scale(mouseDeltaPos, -1.0f/camera.zoom);
+    if (IsKeyDown(KEY_UP)){
+      updateInterval = updateInterval * 0.9f;
+    }
+    if (IsKeyDown(KEY_DOWN)){
+      updateInterval = updateInterval / 0.9f;
+    }
+
+    updateInterval = Clamp(updateInterval, 0.01f, 10.0f);
+
+
+    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+      camera.target = Vector2Scale(mouseDeltaPos, -1.0f / camera.zoom);
       camera.offset = Vector2Add(camera.offset, mouseDeltaPos);
     }
-    
+
     if (IsKeyPressed(KEY_SPACE)) {
       startCellularAutomation = !startCellularAutomation;
     }
@@ -181,9 +198,9 @@ int main() {
       generation = 0;
     }
 
-
-    if (startCellularAutomation) {
+    if (timer > updateInterval && startCellularAutomation) {
       CellularAutomation(grid);
+      timer = 0.0f;
       generation++;
     }
 
@@ -203,10 +220,11 @@ int main() {
     BeginMode2D(camera);
     DrawGrid(grid);
     EndMode2D();
-  
+
     DrawText(TextFormat("Generation: %d", generation), 10, 10, 20, BLACK);
     DrawText(TextFormat("Zoom: %.2f", camera.zoom), 10, 30, 20, BLACK);
     DrawText(TextFormat("FPS: %d", GetFPS()), 10, 50, 20, BLACK);
+    DrawText(TextFormat("Update Interval: %.2f", updateInterval), 10, 70, 20, BLACK);
 
     EndDrawing();
   }
